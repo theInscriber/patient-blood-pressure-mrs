c074ec3d00b397091d40680413df444a3661399f
Samuel Yute <samiyutez@gmail.com>
2021-10-17 20:53:37 +0200

    Add functionality for adding Patients

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,18 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200 uppercase font-semibold">
-                    All Patients
+                <div x-data="{ showAddNewForm: false }" @form-canceled.window="showAddNewForm = false">
+                    <div class="p-6 bg-white border-b border-gray-200 uppercase font-semibold flex justify-between">
+                        <div>
+                            All Patients
+                        </div>
+                        <div clas>
+                            <button class="bg-gray-700 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded" @click="showAddNewForm = true">Add New Patient</button>
+                        </div>
+                    </div>
+                    <div x-show="showAddNewForm">
+                        <livewire:add-new-patient />
+                    </div>
                 </div>
                 <div class="p-6 bg-white border-b border-gray-200">
                     <livewire:patient-table />
