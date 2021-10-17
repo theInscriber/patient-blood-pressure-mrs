bcfa1a6240b57418a47b0b711202a7936a98818d
Samuel Yute <samiyutez@gmail.com>
2021-10-17 20:35:36 +0200

    Add Patients Export to CSV functionality

--- app/Http/Livewire/PatientTable.php
+++ app/Http/Livewire/PatientTable.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Livewire;
 
+use App\Exports\PatientsExport;
 use App\Models\Patient;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\URL;
+use Maatwebsite\Excel\Excel;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 
 class PatientTable extends DataTableComponent
 {
+    protected $listeners = ['refreshComponent' => '$refresh'];
+
+    public array $bulkActions = [
+        'exportSelected' => 'Export',
+    ];
+
     public function columns(): array
     {
         return [
@@ -31,6 +39,21 @@ class PatientTable extends DataTableComponent
                 })
                 ->asHtml(),
         ];
+    }
+
+    /**
+     *
+     */
+    public function exportSelected()
+    {
+        if ($this->selectedRowsQuery()->count() > 0) {
+            $ids = $this->selectedRowsQuery()->pluck('id');
+            return (new PatientsExport($ids))->download(
+                'Patients.csv',
+                Excel::CSV,
+                ['Content-Type' => 'text/csv']
+            );
+        }
     }
 
     public function query(): Builder
