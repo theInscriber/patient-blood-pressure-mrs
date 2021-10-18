1455067f95f905164c66e28e4604b01a765a80b6
Samuel Yute <samiyutez@gmail.com>
2021-10-18 13:03:15 +0200

    Add tests for patients export and blood pressure readings

--- tests/Feature/PatientTest.php
+++ tests/Feature/PatientTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\Feature;
 
-use App\Http\Livewire\AddNewPatient;
-use App\Models\User;
+use App\Http\Livewire\AddNewPatientForm;
+use App\Models\Patient;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
@@ -15,13 +15,27 @@ class PatientTest extends TestCase
 
     public function test_users_can_add_a_new_patient_with_valid_input()
     {
-        $this->actingAs(User::factory()->create());
-        Livewire::test(AddNewPatient::class)
-        $this->assertTrue(true);
+        Livewire::test(AddNewPatientForm::class)
+            ->set('firstName', 'Samuel')
+            ->set('lastName', 'Yute')
+            ->set('sex', 'Male')
+            ->set('birthDate', '01-01-2021')
+            ->set('address', 'Address')
+            ->call('submit');
+
+        $this->assertTrue(Patient::where('first_name', 'Samuel')->where('last_name', 'Yute')->exists());
     }
 
     public function test_users_can_not_add_a_new_patient_with_invalid_input()
     {
-        $this->assertTrue(true);
+        Livewire::test(AddNewPatientForm::class)
+            ->set('sex', 'Other')
+            ->set('birthDate', '01-01-2022')
+            ->set('address', 'Address')
+            ->call('submit')
+            ->assertHasErrors([
+                'firstName' => 'required',
+                'lastName' => 'required',
+            ]);;
     }
 }
